8dda33b7bc6a1f24d323e771988e2077e412ebea
cbogart91 <166065354+cbogart91@users.noreply.github.com>
2024-07-23 11:08:23 -0500

    index models updated

--- models/index.js
+++ models/index.js
@@ -4,15 +4,22 @@ const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
-const sequelize = require('../config/connection');
-
 // Products belongsTo Category
-
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+});
 // Categories have many Products
-
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+});
 // Products belongToMany Tags (through ProductTag)
-
+Product.belongsToMany(Tag, {
+  foreignKey: 'tag_id',
+});
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  foreignKey: 'tag_id',
+});
 
 module.exports = {
   Product,
