2fc97dfb18d87a8ffb7f640c0c58a9890e6c7db5
cbogart91 <166065354+cbogart91@users.noreply.github.com>
2024-07-25 09:22:26 -0500

    final push

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -25,7 +25,7 @@ router.get('/:id', async (req, res) => {
     const productData = await Product.findByPk(req.params.id, {
       include: [{ model: Category }, { model: Tag }],
     });
-    if (!productDataData){
+    if (!productData){
       res.status(404).json( {message: 'No category found with that ID!'});
       return;
     }
